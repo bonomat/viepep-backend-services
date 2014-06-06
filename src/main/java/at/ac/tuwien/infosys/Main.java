61efb43ff3e1f2f99157f0b677bac9d633e78b57
Christoph Hochreiner <ch.hochreiner@gmail.com>
2014-06-06 13:02:42 +0200

    make grizzly also available over the network...

--- src/main/java/at/ac/tuwien/infosys/Main.java
+++ src/main/java/at/ac/tuwien/infosys/Main.java
@@ -5,37 +5,20 @@ import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.NetworkListener;
 
-import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
-import java.net.URI;
 
 
 public class Main {
 
-    private static int getPort(int defaultPort) {
-        //grab port from environment, otherwise fall back to default port 9998
-        String httpPort = System.getProperty("jersey.test.port");
-        if (null != httpPort) {
-            try {
-                return Integer.parseInt(httpPort);
-            } catch (NumberFormatException e) {
-            }
-        }
-        return defaultPort;
-    }
 
-    private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://localhost/").port(getPort(8080)).build();
-    }
 
-    public static final URI BASE_URI = getBaseURI();
-    
     protected static HttpServer startServer() throws IOException {
         ResourceConfig resourceConfig = new PackagesResourceConfig("at.ac.tuwien.infosys");
 
         System.out.println("Starting grizzly2...");
-        return GrizzlyServerFactory.createHttpServer(BASE_URI, resourceConfig);
+        return GrizzlyServerFactory.createHttpServer("http://0.0.0.0:8080", resourceConfig);
     }
     
     public static void main(String[] args) throws IOException {
@@ -48,7 +31,7 @@ public class Main {
         HttpServer httpServer = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...",
-                BASE_URI));
+                "http://localhost:8080"));
         System.in.read();
         httpServer.stop();
         calcThread.interrupt();

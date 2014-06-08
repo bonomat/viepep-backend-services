62475a06705b70f2807ab54346ee2a4396992cf8
Christoph Hochreiner <ch.hochreiner@gmail.com>
2014-06-08 19:39:45 +0200

    added taskconfiguration

--- src/main/java/at/ac/tuwien/infosys/Tasks.java
+++ src/main/java/at/ac/tuwien/infosys/Tasks.java
@@ -11,9 +11,16 @@ public class Tasks {
 
 
     public Tasks() {
-        tasks.put(1, new Task(10.0, 60.0));
-        tasks.put(2, new Task(15.0, 40.0));
-        tasks.put(3, new Task(55.0, 50.0));
+        tasks.put(1, new Task(5.0, 30.0));
+        tasks.put(2, new Task(10.0, 80.0));
+        tasks.put(3, new Task(20.0, 300.0));
+        tasks.put(4, new Task(25.0, 100.0));
+        tasks.put(5, new Task(50.0, 10.0));
+        tasks.put(6, new Task(100.0, 20.0));
+        tasks.put(7, new Task(120.0, 40.0));
+        tasks.put(8, new Task(150.0, 20.0));
+        tasks.put(9, new Task(200.0, 60.0));
+        tasks.put(10, new Task(250.0, 30.0));
 
     }
 

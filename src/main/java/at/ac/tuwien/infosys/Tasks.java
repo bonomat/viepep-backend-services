2973cb9d6794f76a21378971f293d0bb7552a1b7
Philipp Hoenisch <philipp@hoenisch.at>
2014-08-27 10:43:29 +0200

    updated tasks

--- src/main/java/at/ac/tuwien/infosys/Tasks.java
+++ src/main/java/at/ac/tuwien/infosys/Tasks.java
@@ -11,13 +11,13 @@ public class Tasks {
 
 
     public Tasks() {
-        tasks.put(1, new Task(5.0, 30.0));
-        tasks.put(2, new Task(10.0, 80.0));
-        tasks.put(3, new Task(15.0, 120.0));
-        tasks.put(4, new Task(30.0, 100.0));
-        tasks.put(5, new Task(45.0, 10.0));
-        tasks.put(6, new Task(55.0, 20.0));
-        tasks.put(7, new Task(70.0, 40.0));
+        tasks.put(1, new Task(15.0, 30.0));
+        tasks.put(2, new Task(20.0, 80.0));
+        tasks.put(3, new Task(30.0, 120.0));
+        tasks.put(4, new Task(50.0, 100.0));
+        tasks.put(5, new Task(55.0, 10.0));
+        tasks.put(6, new Task(65.0, 20.0));
+        tasks.put(7, new Task(75.0, 40.0));
         tasks.put(8, new Task(125.0, 20.0));
         tasks.put(9, new Task(125.0, 60.0));
         tasks.put(10, new Task(190.0, 30.0));
